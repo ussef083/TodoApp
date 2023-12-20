10ea582f1b57c1cbee44ae9c4ebc4889ec646dfd
youssef wardi <youssefwardi083@gmail.com>
2023-12-20 02:13:40 +0100

    v1.0

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const deleteTaskrouter = require('./routes/deleteTask');
 require('dotenv').config();
 
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://todo-app-by-youssef-wardi.vercel.app/',
+        credentials: true,
+        methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    }
+));
 app.use(express.json());
 app.use('/tasks', getTaskrouter);
 app.use('/tasks', updateTaskrouter);
